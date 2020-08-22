692ae9db7b3cd3a4b1168de48eb6910cc324396a
Sett Sarverott <sarverott@gmail.com>
2020-08-22 11:07:19 +0200

    index.php into index.html

---
+++ index.js
@@ -0,0 +1,12 @@
+const http=require("http"),
+  fs=require("fs"),
+  url=require("url"),
+  path=require("path"),
+  os=require("os"),
+  child_process=require("child_process")
+
+class RokitaServer{
+  constructor(){
+    
+  }
+}
