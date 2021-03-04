e13adacc55388b50248552269b634dbea2507a29
Sett Sarverott <sarverott@gmail.com>
2021-03-04 21:15:08 +0100

    RUN_SERVER funct, renaming CREATETHREAD

--- index.js
+++ index.js
@@ -2,17 +2,36 @@ const http=require("http"),
   fs=require("fs"),
   url=require("url"),
   path=require("path"),
-  os=require("os"),
-  child_process=require("child_process")
+  child_process=require("child_process");
 
 class Rokita_server{
   constructor(){
 
   }
-  static CREATETHREAD(){
+  static CREATE_THREAD(){
+    
+  }
+  static RESOLVE_PATH(){
 
   }
-
+  static RUN_SERVER(host, port){
+    http.createServer(function(request, response){
+      var input="";
+      request.on('data',function(data){
+        input+=data;
+      });
+      request.on('err',function(err){
+        debugOutput(`worker ${process.pid} error- ${err}`);
+      });
+      request.on('end',function(){
+        raportVisit("ok", input, request, nettHook);
+        sendDoc(response, nettHook);
+      });
+    }).listen({
+      port,
+      host
+    });
+  }
 }
 class Rokita_file_manager{
 
